22f71a2ce3c52653ba96ab5e51cba9a443266a65
mwortegam <mwortegam@gmail.com>
2021-09-06 13:21:05 -0500

    Calculator styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.scss';
 import { Container,
@@ -15,6 +15,7 @@ function App() {
   const [ qtyPerson, setQtyPerson ] = useState(1)
   const [ resultValues, setResultValues] = useState({ tipPerPerson:0, totalPerPerson:0 })
 
+
   function handleInputTotal(event){
     let total = event.target.value
     setTotal(total)
@@ -30,85 +31,97 @@ function App() {
     const tip = Number(rawTip)
     console.log(total, qtyPerson)
     console.log(tip)
-    let tipPerPerson = total*tip/qtyPerson
-    let totalPerPerson = total/qtyPerson + tipPerPerson
-    setResultValues({ tipPerPerson, totalPerPerson })
-
+      // let tipPerPerson = total*tip/qtyPerson
+      // let totalPerPerson = total/qtyPerson + tipPerPerson
+      // setResultValues({ tipPerPerson, totalPerPerson })
+    setTipPercent(tip)
   }
 
-  return (
-    <div className="fluid-container d-flex align-items-center vh-100">
-      <Container className="inner-container bg-white vh-50">
-        <Row className="p-3 ">
-          <Col xs="12" md="6">
-            <Form>
-              <FormGroup>
-                <Label for="billInput">Bill</Label>
-                <Input type="text" name="total" id="billInput" placeholder="with a placeholder" onChange={ handleInputTotal}/>    
-                </FormGroup>        
-              </Form>
-              <div>
-                Select Tip %
-              </div>
-              <div>
-                <div>
-                  <Button data-tip=".05" data-dewendy onClick={ handleTipInput }>5%</Button>
-                  <Button data-tip=".1" onClick={ handleTipInput }>10%</Button>
-                  <Button data-tip=".15" onClick={ handleTipInput }>15%</Button>
-                  <Button data-tip=".25" onClick={ handleTipInput }>25%</Button>
-                  <Button data-tip=".5" onClick={ handleTipInput }>50%</Button>
-                  <Input placeholder="Custom" onBlur={ handleTipInput }></Input>
-                </div>
-              </div>
-              <Form>
-              <FormGroup>
-                <Label for="peopleInput">Bill</Label>
-                <Input type="text" name="qtyPerson" id="peopleInput" placeholder="with a placeholder" onChange={ handleInputQtyPerson }/>    
-                </FormGroup>        
-              </Form>
-          </Col>
-          <Col xs="12" md="6" className="results-column mt-3 mt-md-0 text-light">
-            <div className="d-flex justify-content-between">
-              <div>
-                <div>Tip Amount</div>
-                <span>/ person</span>
-              </div>
-              <div>
-                <h1>$ { resultValues.tipPerPerson }</h1>
-              </div>
-            </div>
-            <div className="d-flex justify-content-between ">
-              <div>
-                <div>Total</div>
-                <span>/ person</span>
-              </div>
-              <div>
-                <h1>$ { resultValues.totalPerPerson }</h1>
-              </div>
-            </div>
-            <Button>Reset</Button>
+  useEffect(()=>{
+    let tipPerPerson = total*tipPercent/qtyPerson
+    let totalPerPerson = total/qtyPerson + tipPerPerson
+    setResultValues({ tipPerPerson, totalPerPerson })
+  })
+
+  return(
+    <>
+      <Container>
+
+        <Row>
+          <Col xs="12" md="6"  className="logo">
+            <img src="../images/logo.svg" alt="logo" />
           </Col>
         </Row>
+
+        <Row className="card">
+          {/* input */}
+          <Col className="input">
+            <div className="bill-container">
+                <div className="label-wrapper">
+                  <label htmlFor="billInput">Bill</label>
+                </div>
+
+                <div className="inp-wrapper">
+                  <input className="inp-text" type="text" name="total" id="billInput" placeholder="0.0" onChange={ handleInputTotal}/>
+                  <img src="../images/icon-dollar.svg" alt="dollar-icon" />
+                </div>
+            </div>
+
+            <div className="tip-container">
+                <div className="label-wrapper">
+                  <label htmlFor="inp-tip">Select Tip %</label>
+                </div>
+
+                <div className="btn-wrapper">
+                  <Button className="btn" active={tipPercent==.05} data-tip=".05" data-dewendy onClick={ handleTipInput }>5%</Button>
+                  <Button className="btn" data-tip=".1" onClick={ handleTipInput }>10%</Button>
+                  <Button className="btn" data-tip=".15" onClick={ handleTipInput }>15%</Button>
+                  <Button className="btn" data-tip=".25" onClick={ handleTipInput }>25%</Button>
+                  <Button className="btn" data-tip=".5" onClick={ handleTipInput }>50%</Button>
+                  <input className="inp-text" type="text" name="inp-tip" id="inp-tip" placeholder="Custom" onBlur={ handleTipInput }/>
+                </div>
+            </div>
+
+            <div className="people-container">
+                <div className="label-wrapper">
+                    <label htmlFor="inp-people">Number of People</label>
+                    
+                </div>
+
+                <div className="inp-wrapper">
+                  <input className="inp-text" type="text" name="inp-people" id="inp-people" defaultValue={1} onBlur={ handleInputQtyPerson }/>
+                  <img src="../images/icon-person.svg" alt="person-icon" />
+                </div>
+                
+            </div>
+          </Col>
+
+          {/* results */}
+          <Col className="output">
+            <div className="line-wrapper">
+              <div className="line-output">
+                <div className="tittle-wrapper">
+                  <div className="title">Tip Amount</div>
+                  <div className="desc">/ person</div>
+                </div>
+                <div className="value">${ resultValues.tipPerPerson }</div>
+              </div>
+              <div className="line-output">
+                <div className="tittle-wrapper">
+                  <div className="title">Total</div>
+                  <div className="desc">/ person</div>
+                </div>
+                <div className="value">${ resultValues.totalPerPerson }</div>
+              </div>
+            </div>
+            <div className="btn">Reset</div>
+          </Col>
+        </Row>
+
       </Container>
-    </div>
-      
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+    </>
+  )
+  
 }
 
 export default App;
